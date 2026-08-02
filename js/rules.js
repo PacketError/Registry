f023c0c5006e819d5a7d2732ff316a50a4f5831b
Packet Error <packet.error@proton.me>
2026-08-02 22:13:51 +0200

    Add files via upload

---
+++ js/rules.js
@@ -0,0 +1,66 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const intro = document.getElementById('intro-screen');
+
+    if (intro) {
+        setTimeout(() => {
+            intro.classList.add('fade-out');
+
+            setTimeout(() => {
+                intro.style.display = 'none';
+            }, 800);
+            
+        }, 1500);
+    }
+});
+
+    const menuBtn = document.getElementById('menuBtn');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+
+    menuBtn.addEventListener('click', () => {
+      sidebar.classList.toggle('offen');
+      overlay.classList.toggle('aktiv');
+      
+      if (sidebar.classList.contains('offen')) {
+        menuBtn.textContent = 'X CLOSE';
+      } else {
+        menuBtn.textContent = '_> MENU';
+      }
+    });
+
+    overlay.addEventListener('click', () => {
+      sidebar.classList.remove('offen');
+      overlay.classList.remove('aktiv');
+      menuBtn.textContent = '_> MENU';
+    });
+
+    const titleText = "Registry";
+        let isDeleting = false;
+        let currentText = '';
+
+        function typeTabTitle() {
+            if (!isDeleting) {
+                currentText = titleText.substring(0, currentText.length + 1);
+            } else {
+                currentText = titleText.substring(0, currentText.length - 1);
+            }
+
+            document.title = currentText || 'by P.E'; 
+
+            let typeSpeed = isDeleting ? 150 : 250;
+
+            if (!isDeleting && currentText === titleText) {
+                typeSpeed = 2000; 
+                isDeleting = true;
+            } 
+
+            else if (isDeleting && currentText === '') {
+                isDeleting = false;
+                typeSpeed = 500; 
+            }
+
+            setTimeout(typeTabTitle, typeSpeed);
+        }
+        document.addEventListener("DOMContentLoaded", () => {
+            setTimeout(typeTabTitle, 1000);
+        });
